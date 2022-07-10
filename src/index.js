0dbd601c36b922c529443a848bab09e34b0bd4ed
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-07-11 00:29:37 +0100

    Rendered the goelocation on the UI

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function App() {
-    window.navigator.geolocation.getCurrentPosition(
-        (position) => console.log(position),
-        (err) => console.log(err)
-    )
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state =  {lat: null };
+
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                // To update our state, we called setState!!! 
+                this.setState({ lat: position.coords.latitude });
+            },
+            (err) => console.log(err)
+            )
+            
+        };
+    // React says we have to define render
+    render() {
   return (
-    <div>Hello there</div>
+    <div>Latitude: {this.state.lat} </div>
   )
+    }
 }
 
 ReactDOM.render(
