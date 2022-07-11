d4a27e46deb38a3a8c12aebdb79545addae09273
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-07-11 05:03:21 +0100

    The file that renders it to the browser.

--- src/index.js
+++ src/index.js
@@ -1,29 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 class App extends React.Component {
     constructor(props) {
         super(props);
-        this.state =  {lat: null };
 
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => {
-                // To update our state, we called setState!!! 
-                this.setState({ lat: position.coords.latitude });
-            },
-            (err) => console.log(err)
-            )
-            
+        this.state =  {lat: null, errorMessage: '' };
         };
+
+        
+
+        componentDidMount() {
+            window.navigator.geolocation.getCurrentPosition(
+                (position) => this.setState({ lat: position.coords.latitude }),
+                (err) => this.setState({ errorMessage: err.coords.latitude })
+                )
+        }
+
+        renderContent() {
+            if (this.state.errorMessage && !this.state.lat) {
+                return <div>Error: {this.state.errorMessage}</div>
+            } 
+            
+            if (!this.state.errorMessage && this.state.lat) {
+                return <SeasonDisplay lat={this.state.lat}/> 
+            }
+    
+            return <Spinner message="Please, accept location request" />
+        }
     // React says we have to define render
     render() {
-  return (
-    <div>Latitude: {this.state.lat} </div>
-  )
+      return (
+        <div className="border red">
+            {this.renderContent()}
+        </div>
+
+      )
     }
 }
 
 ReactDOM.render(
 <App />,
 document.querySelector('#root')
-)
+) 
